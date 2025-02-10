625385cfd1e8127e6e307657dd76a4ac60aa24bc
MeeraBhise <bisemeera1998@gmail.com>
2025-02-10 22:44:48 +0530

    Add files via upload
    add jenkins repository

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main{
+
+    public  static void main(String args[]) {
+  System.out.println("this ia java program");
+  System.out.println("this is output of java program ");
+    }
+}
